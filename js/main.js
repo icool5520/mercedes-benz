f00e230bfbfb036358808fbab7461cf68f1c9b89
icool5520 <icool5520@gmail.com>
2022-02-20 00:47:49 +0200

    +content -adaptive

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
-$(function(){
-
+$(function () {
+  $(".design-slider").slick({
+    dots: false,
+    slidesToShow: 4,
+    centerMode: true,
+    variableWidth: true,
+    prevArrow:
+      '<img src="/images/Arrow 1.svg" alt="" class="arrow arrow-left">',
+    nextArrow:
+      '<img src="/images/Arrow 2.svg" alt="" class="arrow arrow-left">',
+  });
 });
